3ef9a9875a845b75b910606da13c2dce287eb412
German <gnrg_74@hotmail.com>
2022-09-19 12:28:07 -0500

    Se agrego conexion a la bd

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const {conexion} = require('./db');
 dotenv.config();
 
 
 const app = express();
-
 const PORT = process.env.PORT;
+
+conexion.connect(error=>{
+    if(error)throw error
+    else{
+        console.log('Conexion exitosa');
+    }
+});
 
 app.listen(PORT,  ()=>{
     console.log(`El servidor esta escuchando en el puerto ${PORT}`);
 });
+
