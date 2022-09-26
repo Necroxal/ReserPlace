194646f2491f2d08bf916da1be5a40bd82804e79
German <gnrg_74@hotmail.com>
2022-09-26 14:10:48 -0500

    update changes

---
+++ apiServices/users/controller.js
@@ -0,0 +1,33 @@
+const db = require('../../db');
+const User = db.User;
+const response = require('../../utils/response');
+
+
+module.exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.email || !req.body.password || !req.body.name || !req.body.lastname ||!req.body.phone ||!req.body.state) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+      return;
+    }
+  
+    // Create a Tutorial
+    const user = {
+      email: req.body.email,
+      password: req.body.password,
+      name: req.body.name,
+      lastname: req.body.lastname,
+      phone: req.body.phone,
+      state: req.body.state
+    };
+  
+    // Save Tutorial in the database
+    User.create(user)
+      .then(data => {
+        response.succes(req,res,data,201);;
+      })
+      .catch(err => {
+        response.error(req,res,'Internal error',500,err);
+      });
+  };
