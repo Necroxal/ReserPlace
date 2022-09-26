3a58567c181daba9b8c99f63a60ffd48837b30f7
German <gnrg_74@hotmail.com>
2022-09-26 14:00:09 -0500

    Changes for connection and add sequelize conn

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express'); //?Server express
 const app = express();
 const dotenv = require('dotenv'); //?For environment varibales in file .env
-const {connect} = require('./db');//?Connection for bd in mysql
+const db = require("./db");
 const routes = require("./routes/routes"); //? Import routes 
 
 //? use dotenv
 dotenv.config(); 
 
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended: false}) );
+db.sequelize.sync({force:true})
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 
-//?Use the route of file routes.js
 routes(app); 
-
-connect();
-
 app.listen(PORT,  ()=>{
     console.log(`El servidor esta escuchando en el puerto ${PORT}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
-const user = require('../apiServices/users/routes');
+const places = require('../apiServices/places/routes');
 
 //&Fucntion to manage routes
 //?The parameter is sent in index.js (main project)
 const router = (app)=>{
-    app.use('/user',user)
+    app.use('/places', places);
 }
 module.exports = router;
