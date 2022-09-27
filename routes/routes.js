2c7d985c4f26fe5d1ed2c0fefc9534fba82e0fc9
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-09-26 20:47:20 -0500

    Updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,14 @@
 const places = require('../apiServices/places/routes');
 const users = require("../apiServices/users/routes");
+const {verifyToken} = require("../apiServices/users/controller");
+const cookieParser = require("cookie-parser");
 
 //&Fucntion to manage routes
 //?The parameter is sent in index.js (main project)
 const router = (app)=>{
+    app.use(cookieParser());
     app.use('/place',places);
-    app.use("/", users);
+    app.use("/", users, verifyToken);
 }
 
 module.exports = router;
