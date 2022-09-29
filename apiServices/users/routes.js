088e0a461536dcf53c27801ce354d38449e1750f
German <gnrg_74@hotmail.com>
2022-09-29 13:34:58 -0500

    Elimated users

--- apiServices/users/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controller = require('./controller');
-const router = express.Router();
-
-//Agregar alumno
-router.post('/', controller.create);
-
-module.exports = router;
