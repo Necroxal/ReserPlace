2794f1acfe4817ea7d01978a766fe2db39891a5c
German <gnrg_74@hotmail.com>
2022-09-29 16:27:19 -0500

    Changes for return image name

--- middleware/multer.js
+++ middleware/multer.js
@@ -11,7 +11,7 @@ const uploadImage = () => {
             const mimetype = fileTypes.test(file.mimetype);
             const extname = fileTypes.test(path.extname(file.originalname));
             if (mimetype && extname) {
-                return cb(null, `${Date.now()}${path.extname(file.originalname)}`)
+                return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
               }
             else{
                 cb("Error: invalid image format");
